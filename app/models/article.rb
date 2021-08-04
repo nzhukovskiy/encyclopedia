d066037e7c51678bd658dad0d5c5a623769e4ea8
Nikita Zhukovskiy <zhukovskiy.sfedu@gmail.com>
2021-08-04 23:10:26 +0300

    12 commit

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,6 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
-  validates :birthdate, presence: true
 
   def get_resources
     JSON.parse(self.resources)
