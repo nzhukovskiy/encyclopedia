b2fb355ccb7698e977eb88741d33e5208834418e
Nikita Zhukovskiy <zhukovskiy.sfedu@gmail.com>
2021-10-12 19:46:14 +0300

    fixed coordinates & elevation update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.coordinates != ""
-      @article.elevation = set_elevation(@article)
+      @article.elevation = set_elevation
     end
     if @article.save
       redirect_to @article
@@ -29,8 +29,8 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-    if @article.coordinates != ""
-      @article.elevation = set_elevation(@article)
+    if params.require(:article).permit(:coordinates) != ""
+      @article.elevation = set_elevation
     end
 
     if @article.update(article_params)
@@ -49,9 +49,9 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:title, :body, :birthdate, :deathdate, :birth_country, :birth_place, :death_country, :death_place, :resources, :duties, :columns_data, :coordinates)
   end
-  def set_elevation (article)
+  def set_elevation
     coordinates = []
-    coordinates += article.coordinates.split(%r{,\s*})
+    coordinates += params.require(:article).permit(:coordinates)["coordinates"].split(%r{,\s*})
     res = helpers.elevation_finder("https://api.opentopodata.org/v1/test-dataset?locations=" + coordinates[0] + ',' + coordinates[1])
   end
 end
