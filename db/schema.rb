c88f222273b27745617c77060996b06a80e84792
Nikita Zhukovskiy <zhukovskiy.sfedu@gmail.com>
2022-01-23 18:44:30 +0300

    Added users & sessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_12_160736) do
+ActiveRecord::Schema.define(version: 2022_01_23_140010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 2021_10_12_160736) do
     t.text "columns_data", default: "[]"
     t.text "coordinates", default: ""
     t.float "elevation"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
