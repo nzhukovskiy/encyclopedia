e4f15ee6f25b9a7b35ee01759c3c3ad5e011eae5
Nikita Zhukovskiy <zhukovskiy.sfedu@gmail.com>
2022-01-30 18:47:59 +0300

    Added index & +hyperlinks to duties

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_27_092628) do
+ActiveRecord::Schema.define(version: 2022_01_30_151931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2022_01_27_092628) do
     t.text "columns_data", default: "[]"
     t.text "coordinates", default: ""
     t.float "elevation"
+    t.index ["title"], name: "index_articles_on_title", unique: true
   end
 
   create_table "comments", force: :cascade do |t|
